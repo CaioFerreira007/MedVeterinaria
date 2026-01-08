130dfd1891775f31de7a6fa0769c787bb50e1c2e
Caio Gustavo <caiogggustavo49@gmail.com>
2026-01-07 22:56:46 -0300

    fase teste 0.4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 import SEOHead from "../components/SEOHead";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/navbar";
 import { BUSINESS_INFO } from "../constants/businessinfo";
 import { useContactForm } from "../hooks/usecontactform";
 import { getWhatsAppLink } from "../services/whatsappService";
